7a16e77f61c09bf7cfcd4ec305d12d7d0286692a
cayan701 <cayan701@gmail.com>
2025-01-21 20:58:04 +0530

    created swagger client

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,14 @@
 // src/app.ts
 import express, { json, urlencoded } from "express";
 import { RegisterRoutes } from "../build/routes";
+import swaggerUi from "swagger-ui-express";
+import { Response as ExResponse, Request as ExRequest } from "express";
 
 export const app = express();
+
+app.use("/docs", swaggerUi.serve, async (_req: ExRequest, res: ExResponse) => {
+  res.send(swaggerUi.generateHTML(await import("../build/swagger.json")));
+});
 
 // Use body parser to read sent json payloads
 app.use(
